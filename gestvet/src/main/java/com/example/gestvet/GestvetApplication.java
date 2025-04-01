0382a48dff489d8bff5cd0fc5873dd8e148c984c
gguerrag <clever.uno@hotmail.com>
2025-03-31 22:57:51 -0300

    cambios

---
+++ gestvet/src/main/java/com/example/gestvet/GestvetApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestvetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestvetApplication.class, args);
+	}
+
+}
---
+++ gestvet/src/test/java/com/example/gestvet/GestvetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestvet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestvetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
